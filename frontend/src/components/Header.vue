c3893faba8d77c09e2a0cfd3555e4eeb5aca5a49
Matteus <matteusm@stud.ntnu.no>
2025-04-24 14:56:18 +0200

    Add buttons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,15 +1,38 @@
 <template>
-    <header class= "p-4 bg-gray-800 text-white">
-        <h1 class="text-xl font-bold"> Krisefikser </h1>
-        <nav>
-            <a href="/" class="mr-4"> Hjem </a>
-            <a href="/login"> Logg inn </a>
-        </nav>
-    </header>
+
+
+  <header class="bg-blue-600 text-white p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <h1 class="text-2xl font-bold">Krisefikser.no</h1>
+      <nav class="space-x-4">
+        <a href="/" class="hover:underline">Hjem</a>
+        <a href="/login" class="hover:underline">Logg inn</a>
+      </nav>
+    </div>
+  </header>
+
+  <button>Default Button</button>
+<a href="#" class="button">Link Button</a>
+<button class="button">Button</button>
+<input type="button" class="button" value="Input Button">
+
 </template>
 
-<script>
-export default {
-    name: 'Header'
-}
+<script lang="ts" setup>
+
 </script>
+
+<style>
+.button {
+  background-color: #04AA6D;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>Home</div>
+  <div>
+  <Header />
+  <main class="p-4">
+    <h2>Dette er hjemmesiden.</h2>
+  </main>
+  </div>
 </template>
 
 <script lang="ts" setup>
+  import Header from '@/components/Header.vue'
+
   
 </script>
