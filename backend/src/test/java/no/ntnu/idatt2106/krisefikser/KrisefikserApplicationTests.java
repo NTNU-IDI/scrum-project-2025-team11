afc066a4aff1fec34890f96356e228bc0e2f5b1f
Garv Sood <garvs@stud.ntnu.no>
2025-04-24 09:46:10 +0200

    Initalize backend project

---
+++ backend/src/test/java/no/ntnu/idatt2106/krisefikser/KrisefikserApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2106.krisefikser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrisefikserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
