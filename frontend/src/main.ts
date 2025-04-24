8d777d7cea9412320bbb8bac98f51d0a36048da8
Therese Synnøve Rondeel <thereron@stud.ntnu.no>
2025-04-24 14:57:09 +0200

    Add import of base.css and main.css

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,8 @@ import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
+import "./assets/base.css"
+import "./assets/main.css"
 import router from "./router/index.ts";
 
 const app = createApp(App);
