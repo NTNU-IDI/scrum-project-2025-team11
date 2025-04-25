5d3e556a2a9a0981caa490e8204768b1297ef6ab
Madeleine Negård <madelne@stud.ntnu.no>
2025-04-25 10:16:16 +0200

    View single item details

--- frontend/src/components/household/ViewSingleArticleComponent.vue
+++ frontend/src/components/household/ViewSingleArticleComponent.vue
@@ -1,20 +1,53 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { useItemTypeStore } from '@/stores/item';
 
-const articles = ref([
-    { id: 1, name: 'Hermetiske tomater', quantity: 1, unit: 'kg' , expirationDate: new Date('2025-12-01') },
-    { id: 2, name: 'Hermetiske tomater', quantity: 5, unit: 'kg', expirationDate: new Date('2026-10-01') },
+const itemTypeStore = useItemTypeStore();
+const itemTypeId = itemTypeStore.id
+
+// TODO: get household items by type
+// const items = getHouseholdItems(itemTypeId)
+
+const items = ref([
+    { name: 'Hermetiske tomater', quantity: 6, unit: 'kg', expirationDate: new Date('2026-10-01') },
+    { name: 'Hermetiske tomater', quantity: 1, unit: 'kg', expirationDate: new Date('2026-05-01') },
 ]);
 </script>
 <template>
-    <h1>Informasjon om artikkel</h1>
-    <div class="supply-container">
-        <h2>{{ articles[0].name }}</h2>
-        <div v-for="article in articles" :key="article.id">
+    <h1 class="medium-header">Informasjon om artikkel</h1>
+    <div class="grey-container">
+        <h2 class="small-header">{{ items[0].name }}</h2>
+        <div v-for="item in items" :key="item.expirationDate.toLocaleDateString">
             <div class="article-card">
-                <p>{{ article.quantity }} {{ article.unit }}</p>
-                <h2>{{ article.expirationDate.toISOString }}</h2>
+                <p>{{ item.quantity }} {{ item.unit }}</p>
+                <div class="info">
+                    <h2>
+                        <span class="grey-text">Utløpsdato:</span> {{ item.expirationDate.toLocaleDateString() }}
+                    </h2>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .grey-container {
+        background-color: var(--light-blue);
+    }
+
+    .small-header {
+        color: white;
+        font-size: var(--font-size-large);
+        font-weight: normal;
+        text-align: right;
+    }
+
+    .article-card {
+        border: 2px solid white; 
+        background-color: white;
+    }
+
+    .grey-text {
+        color: grey;
+    }
+</style>
