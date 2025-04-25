331019a500d4ba68bf19abe2aba54b5fc459d026
Madeleine Negård <madelne@stud.ntnu.no>
2025-04-25 12:07:02 +0200

    Delete button in edit mode

--- frontend/src/components/household/ViewSuppliesComponent.vue
+++ frontend/src/components/household/ViewSuppliesComponent.vue
@@ -2,48 +2,84 @@
 import { ref } from 'vue';
 import { useItemTypeStore } from '@/stores/item';
 
-const itemStore = useItemTypeStore();
-
-// TODO: get total number of items by type
-// const items = getItemsByType()
-
+// Remove later
 const items = ref([
     { id: 1, name: 'Vann', quantity: 10, unit: 'liter'},
     { id: 2, name: 'Hermetiske tomater', quantity: 6, unit: 'kg'},
     { id: 3, name: 'Medisin', quantity: 20, unit: 'tabletter'},
+    { id: 6, name: 'Hermetiske bønner', quantity: 1, unit: 'kg'},
+    { id: 7, name: 'Havregryn', quantity: 1, unit: 'kg'},
+    { id: 8, name: 'Ris', quantity: 1, unit: 'kg'},
+    { id: 9, name: 'Pasta', quantity: 1, unit: 'kg'},
 ]);
 
+const itemStore = useItemTypeStore();
+// Id of the selected item type
 const selectedTypeId = ref<number | null>(null)
+// Is edit mode enabled
+const isEditMode = ref(false);
 
+// TODO: get total number of items by type
+// const items = getItemsByType()
+
+// Choose an item in the supply
 const chooseItemType = (itemTypeId: any) => {
     selectedTypeId.value = itemTypeId;
     itemStore.setItemType(itemTypeId);
 }
+
+// Toggle edit mode
+const toggleEditMode = () => {
+    itemStore.toggleEditMode();
+    isEditMode.value = !isEditMode.value;
+}
 </script>
 <template>
     <h1 class="medium-header">Beredskapslager</h1>
+
     <div class="grey-container">
-        <div 
-        v-for="item in items" 
-        :key="item.id" 
-        @click="chooseItemType(item.id)"
-        :class="['article-card', { active: item.id === selectedTypeId }]"
-        >
-            <div class="quantity">{{ item.quantity }} {{ item.unit }}</div>
-            <div class="info">
-                <h2>{{ item.name }}</h2>
+        <div v-for="item in items" :key="item.id" class="item-card">
+            <div v-if="!isEditMode" class="delete-button">X</div>
+            <div :class="['article-card', { active: item.id === selectedTypeId }]" @click="chooseItemType(item.id)">
+                <div class="quantity">{{ item.quantity }} {{ item.unit }}</div>
+                <div class="info">
+                    <h2>{{ item.name }}</h2>
+                </div>
             </div>
         </div>
     </div>
-</template>
 
+    <button :class="['dark-button', { active: isEditMode }]" @click="toggleEditMode()">
+        {{ isEditMode ? 'Large' : 'Endre lager' }}
+    </button>
+</template>
 <style scoped>
+    .quantity {
+        display: flex;
+        align-items: center;
+    }
+
+    .edit-input {
+        background-color: transparent;
+        border: none;
+        color: var(--darkest-blue);
+        font-size: var(--font-size-medium);
+        width: 100%;
+    }
+
+    #quantity-input {
+        max-width: 1.5rem;
+    }
+
+    .dark-button.active {
+        background-color: var(--good-green);
+    }
     .article-card.active, .article-card:hover {
         background-color: var(--light-blue); 
         color: white;
     }
 
-    .article-card.active .quantity, .article-card:hover .quantity {
+    .article-card.active .quantity, .article-card:hover .quantity, .article-card.active .edit-input, .article-card:hover .edit-input {
         color: white;
         background-color: transparent;
     }
