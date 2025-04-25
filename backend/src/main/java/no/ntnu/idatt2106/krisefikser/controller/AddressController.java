109a3199da263337fb230729b3ebe1831f1178a5
Garv Sood <garvs@stud.ntnu.no>
2025-04-25 14:12:51 +0200

    Implement Address DTOs for request and response; update AddressService to handle DTOs in save method

--- backend/src/main/java/no/ntnu/idatt2106/krisefikser/controller/AddressController.java
+++ backend/src/main/java/no/ntnu/idatt2106/krisefikser/controller/AddressController.java
@@ -20,6 +20,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import no.ntnu.idatt2106.krisefikser.dto.AddressRequestDTO;
+import no.ntnu.idatt2106.krisefikser.dto.AddressResponseDTO;
 import no.ntnu.idatt2106.krisefikser.model.Address;
 import no.ntnu.idatt2106.krisefikser.service.AddressService;
 
@@ -97,12 +99,11 @@ public class AddressController {
     @ApiResponse(responseCode = "400", description = "Invalid address data, for example, missing required fields")
   })
   @PostMapping
-  public ResponseEntity<Address> createAddress(
+  public ResponseEntity<AddressResponseDTO> createAddress(
     @Parameter (description = "Address object to be created", required = true)
-    @RequestBody Address address) {
+    @RequestBody AddressRequestDTO address) {
     try {
-      
-      Address createdAddress = addressService.save(address);
+      AddressResponseDTO createdAddress = addressService.save(address);
       return ResponseEntity.status(HttpStatus.CREATED).body(createdAddress);
     } catch (Exception e) {
       return ResponseEntity.badRequest().header("Error message", e.getMessage()).build();
