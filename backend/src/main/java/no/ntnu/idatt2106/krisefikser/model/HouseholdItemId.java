b069b10963db0eec8f169574a85818867e5285fc
Gabriel Kjølsrud Bøhn <gabriebo@stud.ntnu.no>
2025-04-28 09:36:30 +0200

    Add HouseholdItem and HouseholdItemId classes with composite key implementation

---
+++ backend/src/main/java/no/ntnu/idatt2106/krisefikser/model/HouseholdItemId.java
@@ -0,0 +1,31 @@
+package no.ntnu.idatt2106.krisefikser.model;
+
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+/**
+ * Composite key class for HouseholdItem entity.
+ * This class is used to represent the composite key consisting of householdId, itemId, and acquiredDate.
+ * 
+ * It implements Serializable and overrides equals and hashCode methods for proper comparison.
+ */
+@Embeddable
+@Data
+public class HouseholdItemId implements Serializable {
+    private Integer householdId;
+    private Integer itemId;
+    private LocalDate acquiredDate;
+
+    public HouseholdItemId(Integer householdId,
+                           Integer itemId,
+                           LocalDate acquiredDate) {
+        this.householdId  = householdId;
+        this.itemId       = itemId;
+        this.acquiredDate = acquiredDate;
+    }
+}
