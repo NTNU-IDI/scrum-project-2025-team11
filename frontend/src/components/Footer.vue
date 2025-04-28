1e12c4c0ce48517837083eb531b6fb3c57e3171c
Matteus <matteusm@stud.ntnu.no>
2025-04-28 11:16:45 +0200

    Style footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 <footer class="p-4 bg-[var(--light-blue)] text-white text-center">
-    <h1 class="text-l font-bold mb-2">Andre tjenester</h1>
+    <h1 class="text-l font-bold mb-2">Andre tjenesterRRRRRRRRRRRR</h1>
     <nav>
       <a href="/" class="mr-4 hover:underline">Hjem</a>
       <a href="/personvern" class="hover:underline">Personvern</a>
@@ -20,4 +20,11 @@ footer {
   background-color: #0d1b2a;
   color: white;
 }
+/* Definerer fargen på overskriften i footeren og flytter den til venstre*/
+footer h1 {
+  color: #ffffff;
+  text-align: left;
+  margin-left: 20px;
+}
+
 </style>
