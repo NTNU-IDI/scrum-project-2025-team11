868a86fed606cbede3bc5bb449b41547fb3c7c72
Madeleine Negård <madelne@stud.ntnu.no>
2025-04-28 14:17:14 +0200

    Household store

---
+++ frontend/src/stores/householdStore.ts
@@ -0,0 +1,32 @@
+import {defineStore} from 'pinia'
+
+interface Household {
+    id: number | null
+    name: string
+    memberCount: number
+    addressId: string
+}
+
+export const householdStore = defineStore('household', {
+    state: () => ({
+        id: null as number | null,
+        name: '',
+        memberCount: 0,
+        addressId: '',
+    }),
+    
+    actions: {
+        setHousehold(household: Household) {
+            this.id = household.id;
+            this.name = household.name;
+            this.memberCount = household.memberCount;
+            this.addressId = household.addressId;
+        },
+        clearHousehold() {
+            this.id = null;
+            this.name = '';
+            this.memberCount = 0;
+            this.addressId = '';
+        },
+    }
+})
