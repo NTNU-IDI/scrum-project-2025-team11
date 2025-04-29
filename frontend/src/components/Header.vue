c0c776a2ad15310c6f1136f2b428b60970cbbfee
Matteus <matteusm@stud.ntnu.no>
2025-04-29 14:50:34 +0200

    Switch from Tailwind to CSS

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,17 @@
 <template>
-<header class="bg-[#1b263b] text-white p-4">
-    <div class="container mx-auto flex justify-between items-center">
-      <div class="flex items-center space-x-4">
-        <img src="../assets/invertednew.png" alt="Logo" class="h-12 w-12" @click="goToHome" style="width: 100px; height: auto;" />
-        <a href="/" class="button other">Hjem</a>
+  <header class="header">
+    <div class="header-container">
+      <div class="header-icons">
+        <img src="../assets/invertednew.png" alt="Logo" class="logo" @click="goToHome" />
+
+        <img src="../assets/ikon/house-line-fill.png" alt="Home" class="icon" @click="goToHome" />
+
+        <img src="../assets/ikon/map-trifold-fill.png" alt="Map" class="icon" @click="goToMap" />
+
+        <img src="../assets/ikon/lightbulb-fill.png" alt="Information" class="icon" @click="goToInformation" />
       </div>
-      <nav class="space-x-4">
+
+      <nav class="nav-buttons">
         <a href="/login" class="button login">Logg inn</a>
       </nav>
     </div>
@@ -15,7 +21,7 @@
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
 const router = useRouter()
-const goToMapView = () => {
+const goToMap = () => {
   router.push('/map')
 }
 const goToHome = () => {
@@ -24,10 +30,54 @@ const goToHome = () => {
 const goToLogin = () => {
   router.push('/login')
 }
+const goToInformation = () => {
+  router.push('/information')
+}
 </script>
 
 <style scoped>
+/* --- Header styling --- */
+.header {
+  background-color: #1b263b;
+  color: white;
+  padding: 16px;
+}
 
+.header-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* --- Logo and icons --- */
+.header-icons {
+  display: flex;
+  align-items: center;
+  gap: 60px;
+}
+
+.logo {
+  height: 100px;
+  width: auto;
+  cursor: pointer;
+}
+
+/* Alle ikoner */
+.icon {
+  height: 55px;
+  width: 55px;
+  cursor: pointer;
+}
+
+/* --- Navigasjonsknapper --- */
+.nav-buttons {
+  display: flex;
+  gap: 32px; /* tilsvarer space-x-8 */
+}
+
+/* --- Knappestil --- */
 .button {
   display: inline-block;
   padding: 10px 20px;
@@ -39,23 +89,19 @@ const goToLogin = () => {
   transition: all 0.4s ease;
   text-decoration: none;
   font-weight: 500;
-  margin: 30px 20px;
-
-  /* Default farger hvis ingen variant settes */
-  /*background-color: rgb(41, 35, 153);*/
   background-color: var(--light-blue);
-  color: rgb(255, 255, 255);
+  color: #ffffff;
 }
 
 /* Login-variant */
 .button.login {
-  background-color: var(--good-green); 
+  background-color: var(--good-green);
   border-color: var(--good-green);
-  color: rgb(255, 255, 255);
+  color: #ffffff;
 }
 
 .button.login:hover {
-  background-color: rgb(255, 255, 255);
+  background-color: #ffffff;
   border-color: var(--good-green);
   color: var(--good-green);
 }
@@ -64,13 +110,12 @@ const goToLogin = () => {
 .button.other {
   background-color: var(--light-blue);
   border-color: var(--light-blue);
-  color: rgb(255, 255, 255);
+  color: #ffffff;
 }
 
 .button.other:hover {
-  background-color: rgb(255, 255, 255);
+  background-color: #ffffff;
   border-color: var(--light-blue);
   color: var(--darkest-blue);
 }
-
 </style>
