bfc8a1ea934b055cb6d956d9d6c39f7eb118c4ac
Garv Sood <garvs@stud.ntnu.no>
2025-04-30 09:22:23 +0200

    Add query methods for event retrieval by name and icon type

--- backend/src/main/java/no/ntnu/idatt2106/krisefikser/repository/EventRepository.java
+++ backend/src/main/java/no/ntnu/idatt2106/krisefikser/repository/EventRepository.java
@@ -1,9 +1,13 @@
 package no.ntnu.idatt2106.krisefikser.repository;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import no.ntnu.idatt2106.krisefikser.dto.EventResponseDTO;
 import no.ntnu.idatt2106.krisefikser.model.Event;
 
 /**
@@ -15,5 +19,17 @@ import no.ntnu.idatt2106.krisefikser.model.Event;
  */
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer>, JpaSpecificationExecutor<Event> {
-  
+  /**
+   * Custom query method to find an event by its name.
+   * @param name the name of the event to search for
+   * @return the event with the specified name, or null if not found
+   */
+  Event findByName(String name);
+
+  /**
+   * Finds events by icon type and sorts them by start time in descending order.
+   * @param iconType the icon type to filter events by
+   * @return a list of events matching the specified icon type, sorted by start time in descending order, or an empty list if none found
+   */
+  List<EventResponseDTO> findByIconTypeOrderByStartTimeDesc(String iconType);
 }
