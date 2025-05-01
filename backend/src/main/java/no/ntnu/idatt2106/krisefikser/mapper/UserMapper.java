66aea68c1ffc6ce66401ff881becffaef7189ee3
augustrb <augustreitanbogseth@gmail.com>
2025-05-01 10:31:31 +0200

    Added a seperate mapper class for user

---
+++ backend/src/main/java/no/ntnu/idatt2106/krisefikser/mapper/UserMapper.java
@@ -0,0 +1,41 @@
+package no.ntnu.idatt2106.krisefikser.mapper;
+
+import no.ntnu.idatt2106.krisefikser.dto.AddressResponseDTO;
+import no.ntnu.idatt2106.krisefikser.dto.HouseholdResponseDTO;
+import no.ntnu.idatt2106.krisefikser.dto.UserResponseDTO;
+import no.ntnu.idatt2106.krisefikser.model.Address;
+import no.ntnu.idatt2106.krisefikser.model.Household;
+import no.ntnu.idatt2106.krisefikser.model.User;
+
+public class UserMapper {
+
+    public static UserResponseDTO toResponseDTO(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setId(user.getId());
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+
+        Household household = user.getHousehold();
+        HouseholdResponseDTO householdDTO = new HouseholdResponseDTO();
+        householdDTO.setId(household.getId());
+        householdDTO.setName(household.getName());
+        householdDTO.setMemberCount(household.getMemberCount());
+
+        Address address = household.getAddress();
+        if (address != null) {
+            AddressResponseDTO addressDTO = new AddressResponseDTO();
+            addressDTO.setId(address.getId());
+            addressDTO.setCity(address.getCity());
+            addressDTO.setStreet(address.getStreet());
+            addressDTO.setPostalCode(address.getPostalCode());
+            addressDTO.setLatitude(address.getLatitude());
+            addressDTO.setLongitude(address.getLongitude());
+
+            householdDTO.setAddress(addressDTO);
+        }
+        return dto;
+    }
+    
+}
