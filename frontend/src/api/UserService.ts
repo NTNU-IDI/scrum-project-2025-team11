f8c727775d167464032ffd1d6a7f46be34db99eb
Madeleine Negård <madelne@stud.ntnu.no>
2025-05-02 14:45:50 +0200

    Methods to delete and create user

--- frontend/src/api/UserService.ts
+++ frontend/src/api/UserService.ts
@@ -1,4 +1,4 @@
-import type { UserResponseDTO } from '@/types/User';
+import type { UserResponseDTO} from '@/types/User';
 import axios from 'axios';
 
 const ITEM_API_URL = 'http://localhost:8080/api/users';
@@ -6,6 +6,20 @@ const ITEM_API_URL = 'http://localhost:8080/api/users';
 export class UserService {
     static async findAll(): Promise<UserResponseDTO[]> {
         const response = await axios.get<UserResponseDTO[]>(ITEM_API_URL);
+        return response.data;
+    }
+
+    static async deleteUser(id: number): Promise<void> {
+        await axios.delete(`${ITEM_API_URL}/${id}`);
+    }
+
+    static async findById(id: number): Promise<UserResponseDTO> {
+        const response = await axios.get<UserResponseDTO>(`${ITEM_API_URL}/${id}`);
+        return response.data;
+    }
+
+    static async create(user: Omit<UserResponseDTO, 'id'>): Promise<UserResponseDTO> {
+        const response = await axios.post<UserResponseDTO>(ITEM_API_URL, user);
         return response.data;
     }
 }
