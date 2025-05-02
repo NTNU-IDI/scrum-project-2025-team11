c427bd817f2771a61923c1acd3dec2d928c1de8c
Madeleine Negård <madelne@stud.ntnu.no>
2025-05-02 13:45:48 +0200

    Toggle new event box

--- frontend/src/components/admin/ManageEventsComponent.vue
+++ frontend/src/components/admin/ManageEventsComponent.vue
@@ -1,26 +1,46 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import EditEventComponent from './manage_events/EditEventComponent.vue';
 import ViewEventsComponent from './manage_events/ViewEventsComponent.vue';
+import NewEventComponent from './manage_events/NewEventComponent.vue';
 
+const showNewEventBox = ref(false);
+const toggleNewEventBox = () => {
+    showNewEventBox.value = !showNewEventBox.value;
+}
 </script>
 <template>
 <div class="header-container"> 
     <h1>Administrere Krisehendelser</h1>
-    <p>Her kan du administrere krisehendelser. </p>
+    <div class="page-container">
+        <p>Her kan du administrere krisehendelser.</p>
+        <button class="dark-button" @click="toggleNewEventBox">+ Opprett ny hendelse</button>
+    </div>  
 </div>   
 <div class="page-container">
     <ViewEventsComponent />
     <EditEventComponent />
+    <div v-if="showNewEventBox">
+        <NewEventComponent @hide-new-event-box="toggleNewEventBox" />
+    </div>
 </div>
 </template>
 <style scoped>
 .header-container {
     margin-left: 2rem;
+    margin-top: 4rem;
 }
 
 .page-container {
     display: flex;
     flex-direction: row;
     margin: 1rem;
+}
+
+.dark-button {
+    height: 3rem;
+    width: 12rem;
+    background-color: var(--orange);
+    margin-left: auto;
 }
 </style>
