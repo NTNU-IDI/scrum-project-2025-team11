daccd11e82675c0db38aa1a6228bb786a17a75c4
Madeleine Negård <madelne@stud.ntnu.no>
2025-05-05 16:35:26 +0200

    Send request with credentials

--- frontend/src/api/EventService.ts
+++ frontend/src/api/EventService.ts
@@ -4,22 +4,22 @@ import type { EventRequestDTO, EventResponseDTO } from '@/types/Event';
 const ITEM_API_URL = "http://localhost:8080/api/events";
 
 export class EventService {
-  static async findAll(): Promise<EventResponseDTO[]> {
-    const response = await axios.get<EventResponseDTO[]>(ITEM_API_URL);
-    return response.data;
-  }
+    static async findAll(): Promise<EventResponseDTO[]> {
+        const response = await axios.get<EventResponseDTO[]>(ITEM_API_URL);
+        return response.data;
+    }
 
-  static async findById(id: number): Promise<EventResponseDTO> {
-    const response = await axios.get<EventResponseDTO>(`${ITEM_API_URL}/${id}`);
-    return response.data;
-  }
+    static async findById(id: number): Promise<EventResponseDTO> {
+        const response = await axios.get<EventResponseDTO>(`${ITEM_API_URL}/${id}`);
+        return response.data;
+    }
 
-  static async findActive(): Promise<EventResponseDTO[]> {
-    const response = await axios.get<EventResponseDTO[]>(
-      `${ITEM_API_URL}/active`
-    );
-    return response.data;
-  }
+    static async findActive(): Promise<EventResponseDTO[]> {
+        const response = await axios.get<EventResponseDTO[]>(
+        `${ITEM_API_URL}/active`
+        );
+        return response.data;
+    }
 
     static async update(id: number, event: EventRequestDTO): Promise<EventResponseDTO> {
         const response = await axios.put<EventResponseDTO>(`${ITEM_API_URL}/${id}`, event);
