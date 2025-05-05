6b4d4f7768af574da5c6100ffb8fe0658342ae97
Hong An Ho <hongah@stud.ntnu.no>
2025-05-05 10:43:53 +0200

    added checkboxes for filtering icon types in IconsOverview component

--- frontend/src/components/map/IconsOverview.vue
+++ frontend/src/components/map/IconsOverview.vue
@@ -8,20 +8,73 @@
     </div>
 
     <transition name="fade">
-      <ul v-show="!isCollapsed" class="map-overview-box-descriptions">
-        <li><span class="map-icon shelter"></span> Tilfluktsrom</li>
-        <li><span class="map-icon assembly_point"></span> Møteplass</li>
-        <li><span class="map-icon medical"></span> Medisinsk hjelp</li>
+      <ul v-show="!isCollapsed" class="checkbox-icons-container">
+        <li>
+          <input type="checkbox" id="shelter" value="shelter" v-model="selectedIcons" />
+          <label for="shelter"><span class="map-icon shelter"></span> Tilfluktsrom</label>
+        </li>
+        <li>
+          <input type="checkbox" id="assembly_point" value="assembly_point" v-model="selectedIcons" />
+          <label for="assembly_point"><span class="map-icon assembly_point"></span> Møteplass</label>
+        </li>
+        <li>
+          <input type="checkbox" id="medical" value="medical" v-model="selectedIcons" />
+          <label for="medical"><span class="map-icon medical"></span> Medisinsk hjelp</label>
+        </li>
       </ul>
     </transition>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { usePointStore } from '@/stores/pointStore';
 
 const isCollapsed = ref(false);
+const selectedIcons = ref<string[]>([])
+
+const pointStore = usePointStore()
+
+watch(selectedIcons, (newIcons) => {
+  // TODO: Call PointStore -> Calls Backend
+})
 </script>
 
 <style scoped>
+.checkbox-icons-container {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  padding-top: 7px;
+}
+
+.checkbox-icons-container li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  width: 100%;
+}
+
+.checkbox-icons-container input[type="checkbox"] {
+  margin: 0;
+  flex-shrink: 0;
+  width: auto;
+  padding: 0;
+}
+
+.checkbox-icons-container label {
+  display: flex;
+  align-items: center;
+  margin-left: 0.5rem;
+  cursor: pointer;
+  flex-grow: 1;
+  font-size: var(--font-size-xsmall);
+}
+
+@media (max-width: 768px) {
+    .checkbox-icons-container label {
+      display: flex;
+      font-size: var(--font-size-xsmall);
+  }
+}
 </style>
