00788c13d6d133756c9b87d00000f827fb63533d
Madeleine Negård <madelne@stud.ntnu.no>
2025-05-05 13:26:37 +0200

    Format date for input date type

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -2,6 +2,10 @@ export function formatDate(date: Date): string {
     return date.toISOString().slice(0, 19);
 }
 
+export function formatDateForInput(dateString: string): string {
+    return dateString.slice(0, 16);
+}
+
 export function formatDateToList(date: Date): [String, String, String] {
     const year = String(date.getFullYear()); 
     const month = String(date.getMonth() + 1).padStart(2, '0');   
