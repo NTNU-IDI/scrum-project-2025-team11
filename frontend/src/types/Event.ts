b4d9328c43a56b109d3b3c5b2612ab7ce2f607b7
Madeleine Negård <madelne@stud.ntnu.no>
2025-05-05 11:48:24 +0200

    Add EventRequestDTO

--- frontend/src/types/Event.ts
+++ frontend/src/types/Event.ts
@@ -1,4 +1,14 @@
-export interface EventRequestDTO {}
+export interface EventRequestDTO {
+    name: string;
+    description: string;
+    iconType: string;
+    startDate: string;
+    endDate?: string; 
+    latitude: number;
+    longitude: number;
+    radius: number;
+    severity: number;
+}
 export interface EventResponseDTO {
     id: number;
     name: string;
