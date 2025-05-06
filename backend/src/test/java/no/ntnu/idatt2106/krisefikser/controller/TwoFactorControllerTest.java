6f6bc377891080668b03074ff34a9125612a26e0
Garv Sood <garvs@stud.ntnu.no>
2025-05-06 13:45:58 +0200

    Added JWT security beans to TwoFactorControllerTest and turned off security filters

--- backend/src/test/java/no/ntnu/idatt2106/krisefikser/controller/TwoFactorControllerTest.java
+++ backend/src/test/java/no/ntnu/idatt2106/krisefikser/controller/TwoFactorControllerTest.java
@@ -3,29 +3,43 @@ package no.ntnu.idatt2106.krisefikser.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.ntnu.idatt2106.krisefikser.dto.TwoFactorConfirmDTO;
 import no.ntnu.idatt2106.krisefikser.dto.TwoFactorRequestDTO;
+import no.ntnu.idatt2106.krisefikser.security.JwtAuthFilter;
+import no.ntnu.idatt2106.krisefikser.security.JwtUtil;
 import no.ntnu.idatt2106.krisefikser.service.TwoFactorCodeService;
 import no.ntnu.idatt2106.krisefikser.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
 @WebMvcTest(TwoFactorController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class TwoFactorControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TwoFactorCodeService twoFactorService;
+
+      @MockitoBean 
+  private JwtUtil jwtUtil;           
+
+  
+  @MockitoBean 
+  private JwtAuthFilter jwtAuthFilter;
 
     @Autowired
     private ObjectMapper objectMapper;
