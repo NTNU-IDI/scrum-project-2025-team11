66aff755d3cbffa7916ed45e4124d36604a93fb1
Garv Sood <garvs@stud.ntnu.no>
2025-05-07 16:14:19 +0200

    Added method to find users by household ID in UserRepository

--- backend/src/main/java/no/ntnu/idatt2106/krisefikser/repository/UserRepository.java
+++ backend/src/main/java/no/ntnu/idatt2106/krisefikser/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package no.ntnu.idatt2106.krisefikser.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -25,5 +26,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    List<User> findByHousehold_Id(int householdId);
 
 }
